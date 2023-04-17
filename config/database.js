022564b5a43561a3673b37d038ef055c3d69e761
Sheel1211 <96932878+Sheel1211@users.noreply.github.com>
2023-04-17 12:38:06 +0530

    changes to path

--- config/database.js
+++ config/database.js
@@ -1,11 +1,12 @@
 import mongoose from "mongoose";
 
-export const connectDatabase=()=>{
-    mongoose.connect(process.env.MONGO_URI).then(c=>{
-        console.log(`Mongodb connected to ${c.connection.host}`);
+export const connectDatabase = () => {
+  mongoose
+    .connect(process.env.MONGO_URI)
+    .then((c) => {
+      console.log(`Mongodb connected to ${c.connection.host}`);
     })
-    .catch((e)=>{
-        console.log(e);
-        console.log("Database not connected");
-    })
-}
+    .catch((e) => {
+      console.log(e);
+    });
+};
