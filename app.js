425a14da395f5975bec1c678535cdcc8be6c87c7
Sheel1211 <96932878+Sheel1211@users.noreply.github.com>
2023-04-18 22:48:39 +0530

    added cors

--- app.js
+++ app.js
@@ -5,19 +5,19 @@ import cookieParser from "cookie-parser";
 import path from "path";
 import cors from "cors";
 export const app=express();
+const corsOptions = {
+  origin: ['http://localhost:3000', 'https://sheelpandya.netlify.app']
+}
+
+app.use(cors(corsOptions));
 
 app.use(express.json({limit:"50mb"}));
 app.use(express.urlencoded({extended:true,limit:"50mb"})); // to send larger size photos
 app.use(cookieParser());
 
 import {userRouter} from "./routes/User.js";
+
 app.use("/api/v1",userRouter);
-
-const corsOptions = {
-  origin: ['http://localhost:3000', 'https://sheelpandya.netlify.app']
-}
-
-app.use(cors(corsOptions));
 
 app.use(express.static(path.resolve("../frontend/build")));
 
